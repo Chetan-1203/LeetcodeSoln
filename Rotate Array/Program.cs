17e27ff3d2d84eb8af37fb290a8099710c15db13
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-09 14:42:47 +0530

    rotate array

---
+++ Rotate Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotate_Array
+{
+    internal class Program
+    {
+
+
+        public static int  [] Rotate(int[] nums, int k)
+        {
+            int l = nums.Length;
+            int[] arr = new int[l];
+            int j = 0;
+            for (int i = l - k; i < nums.Length; i++)
+            { 
+                arr[j]=nums[i];
+                j++;
+
+            }
+            for (int i = 0; i < l-k; i++)
+            {
+                arr[j++]=nums[i];   
+
+            }
+            nums = arr;
+
+            return nums;
+
+            /*foreach (int i in arr)
+            {
+                Console.Write(i.ToString()+ " ");
+            }*/
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 1, 2, 3, 4, 5, 6 };
+            int [] result =Rotate(arr, 3);
+            foreach (var item in result)
+            {
+                Console.Write(item.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squares_of_a_Sorted_Array
+{
+    internal class Program
+    {
+        public static int[] SortedSquares(int[] nums)
+        {
+            List<int> negative = new List<int>();
+            List<int> positive = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= 0)
+                {
+                    negative.Add(nums[i] * nums[i]);
+                }
+                else
+                {
+                    positive.Add(nums[i] * nums[i]);
+                }
+
+            }
+            int[] arr = new int[negative.Count + positive.Count];
+            int j = negative.Count-1;
+            int k = 0;
+            int a = 0;
+            while (j >= 0 && k < positive.Count)
+            {
+                if (negative[j] < positive[k])
+                {
+                    arr[a++] = negative[j--];
+                }
+                else
+                {
+                    arr[a++] = positive[k++];
+                }
+            }
+
+            while (j >= 0)
+            {
+                arr[a++] = negative[j--];    
+            }
+            while(k < positive.Count)
+            {
+                arr[a++]=positive[k++];
+            }
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { -4, -1, 0, 3, 10 };
+           int [] result = SortedSquares(arr);
+            foreach(int res in result)
+            {
+                Console.WriteLine(res.ToString());
+            }
+            Console.ReadLine();
+        }
+
+    }
+    
+   
+
+
+}
