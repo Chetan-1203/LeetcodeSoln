0eb105055a740acedc19a007805280900528de2b
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-12 14:11:47 +0530

    reversewords

---
+++ Reverse words in a string/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_words_in_a_string
+{
+    internal class Program
+
+    {
+        public static  void  ReverseWords(string s)
+        {
+           string[] str = s.Split(' ');
+         
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                string subString = "";
+                for (int j = str[i].Length - 1; j >=0; j--)
+                {
+                    subString += str[i][j];
+                }
+
+                result =result+subString+ " ";
+                result.Trim();
+            }
+            Console.WriteLine(2);
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args)
+        {
+            ReverseWords("Hii I am Chetan");
+            Console.ReadLine();
+        }
+    }
+}
