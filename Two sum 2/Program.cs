4f712c0a98b7166640af3ec3f15bd0ffcd9d7d52
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-12 12:36:04 +0530

    two sum

---
+++ Two sum 2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_sum_2
+{
+    internal class Program
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            var list = new ArrayList();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                    {
+                        list.Add(i);
+                        list.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            return (int[])list.ToArray(typeof(int));
+
+        }
+        public static int[] TwoSum2(int[] numbers, int target)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (result.ContainsKey(numbers[i]))
+                {
+                    return new int[] { result[numbers[i]],i+1 }; 
+                }
+                else if (!result.ContainsKey(target - numbers[i])){
+                    result.Add(target-numbers[i], i+1);  
+                }
+            }
+
+           /* for (int i = 0; i < numbers.Length; i++)
+            {
+                if (result.ContainsKey(numbers[i]) && i != result[numbers[i]])
+                {
+                    return new int[] { i+1, result[numbers[i]]+1};
+                }
+            }*/
+            return new int[] {};
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = {2,7,11,15};
+            int [] result=TwoSum2(arr, 9);
+            foreach (int i in result)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
