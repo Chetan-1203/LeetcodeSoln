a813adfff5b62213f800b4a5ea68ce22e92c8bb5
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-13 16:33:02 +0530

    longest substring

---
+++ Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    public  class Program
+    {
+        
+        public static int LengthOfLongestSubstring(string s)
+        {
+            string result = "";
+            int length = 0;
+            
+            if(s.Length == 0)
+            {
+                return 0;   
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            foreach(char c in s)
+            {
+                var ch = c;
+
+                if (result.Contains(ch))
+                {
+                   
+                    result=result.Substring(result.IndexOf(ch)+1); 
+                }
+                result = result + c;
+                length = Math.Max(length, result.Length);
+            }
+            return length;
+        }
+        static void Main(string[] args)
+        {
+            string a = "pwwkew";
+            
+            Console.WriteLine(LengthOfLongestSubstring(a));
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Remove Nth node from end of list/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Nth_node_from_end_of_list
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public  class Program
+    {
+        public class Solution
+        {
+            public static ListNode RemoveNthFromEnd(ListNode head, int n)
+            {
+                ListNode newNode = new ListNode(0);
+                newNode.next = head;
+                ListNode trav = newNode;
+                ListNode temp = newNode;
+                 
+                while(n > 0)
+                {
+                    trav=trav.next;
+                    n--;
+                }
+
+                while(trav.next != null)
+                {
+                    trav =trav.next;
+                    temp=temp.next;
+                }
+                temp.next = temp.next.next;
+                return newNode.next;
+            }
+            static void Main(string[] args)
+            {
+                ListNode head = new ListNode(1);
+
+                ListNode node1 = new ListNode(2);
+                ListNode node2 = new ListNode(3);
+                ListNode node3 = new ListNode(4);
+                ListNode node4 = new ListNode(5);
+
+
+                head.next = node1;
+                node1.next = node2;
+                node2.next = node3;
+                node3.next = node4;
+
+                ListNode result = RemoveNthFromEnd(head,2);
+
+                ListNode trav = result;
+                while(trav!=null)
+                {
+                    Console.WriteLine(trav.val);
+                    trav=trav.next;
+                }
+
+
+                Console.ReadLine(); 
+            }
+        }
+       
+    }
+}
